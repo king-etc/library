93ebddcb2d68f23a6a9a51f15b50e51dffc29c7e
king-etc <36837931+king-etc@users.noreply.github.com>
2019-08-06 23:37:27 +0800

    Update blake2b.cpp

--- BLAKE2B/blake2b.cpp
+++ BLAKE2B/blake2b.cpp
@@ -1,29 +1,70 @@
+
 #include "blake2b.hpp"
 
 static const uint64_t blake2b_IV[8] =
 {
-	0x6a09e667f3bcc908U, 0xbb67ae8584caa73bU,
-	0x3c6ef372fe94f82bU, 0xa54ff53a5f1d36f1U,
-	0x510e527fade682d1U, 0x9b05688c2b3e6c1fU,
-	0x1f83d9abfb41bd6bU, 0x5be0cd19137e2179U
+	0x6a09e667f3bcc908ULL, 0xbb67ae8584caa73bULL,
+	0x3c6ef372fe94f82bULL, 0xa54ff53a5f1d36f1ULL,
+	0x510e527fade682d1ULL, 0x9b05688c2b3e6c1fULL,
+	0x1f83d9abfb41bd6bULL, 0x5be0cd19137e2179ULL
 };
+
 
 static const uint8_t blake2b_sigma[12][16] =
 {
-	{ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 } ,
-{ 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 } ,
-{ 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4 } ,
-{ 7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8 } ,
-{ 9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13 } ,
-{ 2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9 } ,
-{ 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11 } ,
-{ 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10 } ,
-{ 6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5 } ,
-{ 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13 , 0 } ,
-{ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 } ,
-{ 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 }
+	{  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 } ,
+	{ 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 } ,
+	{ 11,  8, 12,  0,  5,  2, 15, 13, 10, 14,  3,  6,  7,  1,  9,  4 } ,
+	{  7,  9,  3,  1, 13, 12, 11, 14,  2,  6,  5, 10,  4,  0, 15,  8 } ,
+	{  9,  0,  5,  7,  2,  4, 10, 15, 14,  1, 11, 12,  6,  8,  3, 13 } ,
+	{  2, 12,  6, 10,  0, 11,  8,  3,  4, 13,  7,  5, 15, 14,  1,  9 } ,
+	{ 12,  5,  1, 15, 14, 13,  4, 10,  0,  7,  6,  3,  9,  2,  8, 11 } ,
+	{ 13, 11,  7, 14, 12,  1,  3,  9,  5,  0, 15,  4,  8,  6,  2, 10 } ,
+	{  6, 15, 14,  9, 11,  3,  0,  8, 12,  2, 13,  7,  1,  4, 10,  5 } ,
+	{ 10,  2,  8,  4,  7,  6,  1,  5, 15, 11,  9, 14,  3, 12, 13 , 0 } ,
+	{  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15 } ,
+	{ 14, 10,  4,  8,  9, 15, 13,  6,  1, 12,  0,  2, 11,  7,  5,  3 }
 };
-static inline uint64_t load64(const uint8_t *src)
+
+static  void store32(uint8_t* dst, uint32_t w)
+{
+	const union {
+		long one;
+		char little;
+	} is_endian = { 1 };
+
+	if (is_endian.little) {
+		memcpy(dst, &w, sizeof(w));
+	}
+	else {
+		uint8_t* p = (uint8_t*)dst;
+		int i;
+
+		for (i = 0; i < 4; i++)
+			p[i] = (uint8_t)(w >> (8 * i));
+	}
+}
+
+static  void store64(uint8_t* dst, uint64_t w)
+{
+	const union {
+		long one;
+		char little;
+	} is_endian = { 1 };
+
+	if (is_endian.little) {
+		memcpy(dst, &w, sizeof(w));
+	}
+	else {
+		uint8_t* p = (uint8_t*)dst;
+		int i;
+
+		for (i = 0; i < 8; i++)
+			p[i] = (uint8_t)(w >> (8 * i));
+	}
+}
+
+static uint64_t load64(const uint8_t* src)
 {
 	const union {
 		long one;
@@ -47,55 +88,12 @@ static inline uint64_t load64(const uint8_t *src)
 		return w;
 	}
 }
-static inline void store32(uint8_t *dst, uint32_t w)
-{
-	const union {
-		long one;
-		char little;
-	} is_endian = { 1 };
-
-	if (is_endian.little) {
-		memcpy(dst, &w, sizeof(w));
-	}
-	else {
-		uint8_t *p = (uint8_t *)dst;
-		int i;
-
-		for (i = 0; i < 4; i++)
-			p[i] = (uint8_t)(w >> (8 * i));
-	}
-}
-static inline void store64(uint8_t *dst, uint64_t w)
-{
-	const union {
-		long one;
-		char little;
-	} is_endian = { 1 };
-
-	if (is_endian.little) {
-		memcpy(dst, &w, sizeof(w));
-	}
-	else {
-		uint8_t *p = (uint8_t *)dst;
-		int i;
-
-		for (i = 0; i < 8; i++)
-			p[i] = (uint8_t)(w >> (8 * i));
-	}
-}
-static inline uint64_t rotr64(const uint64_t w, const unsigned int c)
+static uint64_t rotr64(const uint64_t w, const unsigned int c)
 {
 	return (w >> c) | (w << (64 - c));
 }
 
-/* Set that it's the last block we'll compress */
-static inline void blake2b_set_lastblock(BLAKE2B_CTX *S)
-{
-	S->f[0] = -1;
-}
-
-/* Initialize the hashing state. */
-static inline void blake2b_init0(BLAKE2B_CTX *S)
+static  void blake2b_init0(BLAKE2B_CTX* S)
 {
 	int i;
 
@@ -105,27 +103,10 @@ static inline void blake2b_init0(BLAKE2B_CTX *S)
 	}
 }
 
-/* init xors IV with input parameter block */
-static void blake2b_init_param(BLAKE2B_CTX *S, const BLAKE2B_PARAM *P)
-{
-	size_t i;
-	const uint8_t *p = (const uint8_t *)(P);
-	blake2b_init0(S);
 
-	/* The param struct is carefully hand packed, and should be 64 bytes on
-	* every platform. */
-	assert(sizeof(BLAKE2B_PARAM) == 64);
-	/* IV XOR ParamBlock */
-	for (i = 0; i < 8; ++i) {
-		S->h[i] ^= load64(p + sizeof(S->h[i]) * i);
-	}
-}
-
-/* Initialize the hashing context.  Always returns 1. */
-int BLAKE2b_Init(BLAKE2B_CTX *c,uint8_t digest_len)
+void blake2b_param_init(BLAKE2B_PARAM* P, unsigned int BLAKE2B_DIGEST_LENGTH)
 {
-	BLAKE2B_PARAM P[1];
-	P->digest_length = digest_len;
+	P->digest_length = BLAKE2B_DIGEST_LENGTH;
 	P->key_length = 0;
 	P->fanout = 1;
 	P->depth = 1;
@@ -136,12 +117,28 @@ int BLAKE2b_Init(BLAKE2B_CTX *c,uint8_t digest_len)
 	memset(P->reserved, 0, sizeof(P->reserved));
 	memset(P->salt, 0, sizeof(P->salt));
 	memset(P->personal, 0, sizeof(P->personal));
-	blake2b_init_param(c, P);
-	return 1;
 }
 
-/* Permute the state while xoring in the block of data. */
-static void blake2b_compress(BLAKE2B_CTX *S, const uint8_t *blocks, size_t len)
+static void blake2b_init_param(BLAKE2B_CTX* S, const BLAKE2B_PARAM* P)
+{
+	size_t i;
+	const uint8_t* p = (const uint8_t*)(P);
+
+	blake2b_init0(S);
+	S->outlen = P->digest_length;
+
+	/* The param struct is carefully hand packed, and should be 64 bytes on
+	 * every platform. */
+	assert(sizeof(BLAKE2B_PARAM) == 64);
+	/* IV XOR ParamBlock */
+	for (i = 0; i < 8; ++i) {
+		S->h[i] ^= load64(p + sizeof(S->h[i]) * i);
+	}
+}
+
+static void blake2b_compress(BLAKE2B_CTX* S,
+	const uint8_t* blocks,
+	size_t len)
 {
 	uint64_t m[16];
 	uint64_t v[16];
@@ -149,24 +146,24 @@ static void blake2b_compress(BLAKE2B_CTX *S, const uint8_t *blocks, size_t len)
 	size_t increment;
 
 	/*
-	* There are two distinct usage vectors for this function:
-	*
-	* a) BLAKE2b_Update uses it to process complete blocks,
-	*    possibly more than one at a time;
-	*
-	* b) BLAK2b_Final uses it to process last block, always
-	*    single but possibly incomplete, in which case caller
-	*    pads input with zeros.
-	*/
+	 * There are two distinct usage vectors for this function:
+	 *
+	 * a) BLAKE2b_Update uses it to process complete blocks,
+	 *    possibly more than one at a time;
+	 *
+	 * b) BLAK2b_Final uses it to process last block, always
+	 *    single but possibly incomplete, in which case caller
+	 *    pads input with zeros.
+	 */
 	assert(len < BLAKE2B_BLOCKBYTES || len % BLAKE2B_BLOCKBYTES == 0);
 
 	/*
-	* Since last block is always processed with separate call,
-	* |len| not being multiple of complete blocks can be observed
-	* only with |len| being less than BLAKE2B_BLOCKBYTES ("less"
-	* including even zero), which is why following assignment doesn't
-	* have to reside inside the main loop below.
-	*/
+	 * Since last block is always processed with separate call,
+	 * |len| not being multiple of complete blocks can be observed
+	 * only with |len| being less than BLAKE2B_BLOCKBYTES ("less"
+	 * including even zero), which is why following assignment doesn't
+	 * have to reside inside the main loop below.
+	 */
 	increment = len < BLAKE2B_BLOCKBYTES ? len : BLAKE2B_BLOCKBYTES;
 
 	for (i = 0; i < 8; ++i) {
@@ -241,22 +238,19 @@ static void blake2b_compress(BLAKE2B_CTX *S, const uint8_t *blocks, size_t len)
 		len -= increment;
 	} while (len);
 }
-
-
-/* Absorb the input data into the hash state.  Always returns 1. */
-int BLAKE2b_Update(BLAKE2B_CTX *c, const unsigned char *data, size_t datalen)
+int blake2b_update(BLAKE2B_CTX* c, const unsigned char* data, size_t datalen)
 {
-	const uint8_t *in = data;
+	const uint8_t* in = data;
 	size_t fill;
 
 	/*
-	* Intuitively one would expect intermediate buffer, c->buf, to
-	* store incomplete blocks. But in this case we are interested to
-	* temporarily stash even complete blocks, because last one in the
-	* stream has to be treated in special way, and at this point we
-	* don't know if last block in *this* call is last one "ever". This
-	* is the reason for why |datalen| is compared as >, and not >=.
-	*/
+	 * Intuitively one would expect intermediate buffer, c->buf, to
+	 * store incomplete blocks. But in this case we are interested to
+	 * temporarily stash even complete blocks, because last one in the
+	 * stream has to be treated in special way, and at this point we
+	 * don't know if last block in *this* call is last one "ever". This
+	 * is the reason for why |datalen| is compared as >, and not >=.
+	 */
 	fill = sizeof(c->buf) - c->buflen;
 	if (datalen > fill) {
 		if (c->buflen) {
@@ -269,9 +263,9 @@ int BLAKE2b_Update(BLAKE2B_CTX *c, const unsigned char *data, size_t datalen)
 		if (datalen > BLAKE2B_BLOCKBYTES) {
 			size_t stashlen = datalen % BLAKE2B_BLOCKBYTES;
 			/*
-			* If |datalen| is a multiple of the blocksize, stash
-			* last complete block, it can be final one...
-			*/
+			 * If |datalen| is a multiple of the blocksize, stash
+			 * last complete block, it can be final one...
+			 */
 			stashlen = stashlen ? stashlen : BLAKE2B_BLOCKBYTES;
 			datalen -= stashlen;
 			blake2b_compress(c, in, datalen);
@@ -287,47 +281,94 @@ int BLAKE2b_Update(BLAKE2B_CTX *c, const unsigned char *data, size_t datalen)
 
 	return 1;
 }
-typedef void *(*memset_t)(void *, int, size_t);
-
-static volatile memset_t memset_func = memset;
-
-void OPENSSL_cleanse(void *ptr, size_t len)
+static void blake2b_set_lastblock(BLAKE2B_CTX* S)
 {
-	memset_func(ptr, 0, len);
+	S->f[0] = -1;
 }
-
-/*
-* Calculate the final hash and save it in md.
-* Always returns 1.
-*/
-int BLAKE2b_Final(unsigned char *md, BLAKE2B_CTX *c)
+#define BLAKE2B_OUTBYTES 32
+int blake2b_final(unsigned char* md, BLAKE2B_CTX * c)
 {
+	uint8_t outbuffer[BLAKE2B_OUTBYTES] = { 0 };
+	uint8_t* target = outbuffer;
+	int iter = (c->outlen + 7) / 8;
 	int i;
+
+	/* Avoid writing to the temporary buffer if possible */
+	if ((c->outlen % sizeof(c->h[0])) == 0)
+		target = md;
 
 	blake2b_set_lastblock(c);
 	/* Padding */
 	memset(c->buf + c->buflen, 0, sizeof(c->buf) - c->buflen);
 	blake2b_compress(c, c->buf, c->buflen);
 
-	/* Output full hash to message digest */
-	for (i = 0; i < 8; ++i) {
-		store64(md + sizeof(c->h[i]) * i, c->h[i]);
-	}
+	/* Output full hash to buffer */
+	for (i = 0; i < iter; ++i)
+		store64(target + sizeof(c->h[i]) * i, c->h[i]);
 
-	OPENSSL_cleanse(c, sizeof(BLAKE2B_CTX));
+	if (target != md)
+		memcpy(md, target, c->outlen);
+
+	memset(c, 0, sizeof(BLAKE2B_CTX));
 	return 1;
 }
-bool BLAKE2b_hash(const unsigned char* data, unsigned int data_len,const unsigned char* out,uint8_t digest_len)
-{
-	
-	if (data == nullptr)
-		return false;
 
-	unsigned char hash[64] = {};
+int blake2b_init_key(BLAKE2B_CTX* c, const BLAKE2B_PARAM* P, const void* key)
+{
+	blake2b_init_param(c, P);
+
+	/* Pad the key to form first data block */
+	{
+		uint8_t block[BLAKE2B_BLOCKBYTES] = { 0 };
+
+		memcpy(block, key, P->key_length);
+		blake2b_update(c, block, BLAKE2B_BLOCKBYTES);
+		memset(block, 0, BLAKE2B_BLOCKBYTES);
+	}
+
+	return 1;
+}
+
+static int blake2b_mac_init(EVP_MAC_IMPL* macctx)
+{
+	/* Check key has been set */
+	if (macctx->params.key_length == 0) {
+		return 0;
+	}
+
+	return blake2b_init_key(&macctx->ctx, &macctx->params, macctx->key);
+}
+
+static int blake2b_mac_update(EVP_MAC_IMPL* macctx, const unsigned char* data,
+	size_t datalen)
+{
+	return blake2b_update(&macctx->ctx, data, datalen);
+}
+
+static int blake2b_mac_final(EVP_MAC_IMPL* macctx, unsigned char* out)
+{
+	return blake2b_final(out, &macctx->ctx);
+}
+
+int blake2b_hash(const unsigned char* message, size_t length, unsigned char* out, unsigned int BLAKE2B_DIGEST_LENGTH)
+{
+	BLAKE2B_PARAM P;
 	BLAKE2B_CTX temp;
-	BLAKE2b_Init(&temp,digest_len);
-	BLAKE2b_Update(&temp, data, data_len);
-	BLAKE2b_Final(hash, &temp);
-	memcpy((unsigned char*)out, hash, digest_len);
-	return true;
+	blake2b_param_init(&P, BLAKE2B_DIGEST_LENGTH);
+	blake2b_init_param(&temp, &P);
+	auto x1 = blake2b_update(&temp, message, length);
+	auto x2 = blake2b_final(out, &temp);
+	return x1 && x2;
+}
+int blake2b_mac_hash(const unsigned char* message, size_t length, unsigned char* out, unsigned char* key, unsigned int key_len, unsigned int BLAKE2B_DIGEST_LENGTH)
+{
+	EVP_MAC_IMPL temp;
+	memcpy(temp.key, key, key_len);
+	blake2b_param_init(&temp.params, BLAKE2B_DIGEST_LENGTH);
+	temp.params.key_length = key_len;
+	bool x = true;
+	x = x && blake2b_mac_init(&temp);
+	x = x && blake2b_mac_update(&temp, message, length);
+	x = x && blake2b_mac_final(&temp, out);
+	return x;
 }
