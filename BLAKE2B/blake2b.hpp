e35f40761956d26133e7c3f0ba1b22f84505ef8e
king-etc <36837931+king-etc@users.noreply.github.com>
2019-08-06 23:36:43 +0800

    Update blake2b.hpp

--- BLAKE2B/blake2b.hpp
+++ BLAKE2B/blake2b.hpp
@@ -1,24 +1,12 @@
-#pragma once
-#include <cstdint>
 #include <iostream>
-#include <cassert>
+#include <assert.h>
+# define BLAKE2B_SALTBYTES     16
+# define BLAKE2B_PERSONALBYTES 16
+# define BLAKE2B_BLOCKBYTES    128
+# define BLAKE2B_KEYBYTES	   64
 
 
-#define BLAKE2B_BLOCKBYTES    128
-#define BLAKE2B_OUTBYTES      64
-#define BLAKE2B_KEYBYTES      64
-#define BLAKE2B_SALTBYTES     16
-#define BLAKE2B_PERSONALBYTES 16
-#define BLAKE2B_DIGEST_LENGTH 64
 
-struct blake2b_ctx_st {
-	uint64_t h[8];
-	uint64_t t[2];
-	uint64_t f[2];
-	uint8_t  buf[BLAKE2B_BLOCKBYTES];
-	size_t   buflen;
-};
-typedef struct blake2b_ctx_st BLAKE2B_CTX;
 struct blake2b_param_st {
 	uint8_t  digest_length; /* 1 */
 	uint8_t  key_length;    /* 2 */
@@ -32,14 +20,35 @@ struct blake2b_param_st {
 	uint8_t  salt[BLAKE2B_SALTBYTES]; /* 48 */
 	uint8_t  personal[BLAKE2B_PERSONALBYTES];  /* 64 */
 };
+struct blake2b_ctx_st {
+	uint64_t h[8];
+	uint64_t t[2];
+	uint64_t f[2];
+	uint8_t  buf[BLAKE2B_BLOCKBYTES];
+	size_t   buflen;
+	size_t   outlen;
+};
+typedef struct blake2b_ctx_st BLAKE2B_CTX;
 typedef struct blake2b_param_st BLAKE2B_PARAM;
+struct evp_mac_impl_st {
+	BLAKE2B_CTX ctx;
+	BLAKE2B_PARAM params;
+	unsigned char key[BLAKE2B_KEYBYTES];
+};
 
 
+typedef struct evp_mac_impl_st EVP_MAC_IMPL;
 
 
+int blake2b_update(BLAKE2B_CTX* c, const unsigned char* data, size_t datalen);
+int blake2b_final(unsigned char* md, BLAKE2B_CTX* c);
+int blake2b_init_key(BLAKE2B_CTX* c, const BLAKE2B_PARAM* P, const void* key);
 
-int BLAKE2b_Init(BLAKE2B_CTX *c, uint8_t digest_len = 64);
-int BLAKE2b_Update(BLAKE2B_CTX *c, const unsigned char *data, size_t datalen);
-int BLAKE2b_Final(unsigned char *md, BLAKE2B_CTX *c);
-bool BLAKE2b_hash(const unsigned char* data, unsigned int data_len, const unsigned char* out, uint8_t digest_len);
 
+static int blake2b_mac_init(EVP_MAC_IMPL* macctx);
+static int blake2b_mac_update(EVP_MAC_IMPL* macctx, const unsigned char* data, size_t datalen);
+static int blake2b_mac_final(EVP_MAC_IMPL* macctx, unsigned char* out);
+
+
+int blake2b_hash(const unsigned char* message, size_t length, unsigned char* out, unsigned int BLAKE2B_DIGEST_LENGTH);
+int blake2b_mac_hash(const unsigned char* message, size_t length, unsigned char* out, unsigned char* key, unsigned int key_len, unsigned int BLAKE2B_DIGEST_LENGTH);
